108a076ab4745d8a97bdeb4dbee9f06d3aaba788
Andrew Dimmer <contactandrewd@gmail.com>
2019-05-19 02:13:12 -0400

    Added new Firebase Functions
    Added the login function, the basic display setup for both clients and businesses, the unfollow function, and the basic add location code.

--- scripts/popup-hopper-profile.js
+++ scripts/popup-hopper-profile.js
@@ -1,7 +1,6 @@
 // Creates a new user account directly after the user signs up.
 function createUserAccount(parameters) {
     if (parameters.type == 0) {
-        three = true;
         var cID = "c-" + generateID(10);
         var newClient = db.collection("clients").doc(cID);
         console.log(cID + " reserved");
@@ -126,6 +125,54 @@ function follow(bID) {
     });
 }
 
+function unfollow(bID) {
+    var cID = document.cookie.substring(document.cookie.indexOf("=")+1);
+    var followingUser = db.collection("clients").doc(cID);
+    db.runTransaction(function(transaction) {
+        // This code may get re-run multiple times if there are conflicts.
+        return transaction.get(followingUser).then(function(userDoc) {
+            if (!userDoc.exists) {
+                console.log("Document does not exist!");
+            }
+            var following = userDoc.data().Following;
+            var unfollowed = [];
+            for (var i = 0; i < following.length; i++) {
+                console.log(following[i].bID);
+                if (following[i].bID.indexOf(bID) < 0) {
+                    unfollowed.push(following[i]);
+                }
+            }
+            transaction.update(followingUser, {Following: unfollowed});
+        });
+    }).then(function() {
+        console.log("Transaction successfully committed!");
+        var followedBusiness = db.collection("businesses").doc(bID);
+        db.runTransaction(function(transaction) {
+            // This code may get re-run multiple times if there are conflicts.
+            return transaction.get(followedBusiness).then(function(businessDoc) {
+                if (!businessDoc.exists) {
+                    console.log("Document does not exist!");
+                }
+                var followers = businessDoc.data().Followers;
+                var unfollowed = [];
+                for (var i = 0; i < followers.length; i++) {
+                    if (followers[i].cID.indexOf(cID) < 0) {
+                        unfollowed.push(following[i]);
+                    }
+                }
+                transaction.update(followedBusiness, {Followers: unfollowed});
+            });
+        }).then(function() {
+            console.log("Transaction successfully committed!");
+            alert("Success!");
+        }).catch(function(error) {
+            console.log("Transaction failed: ", error);
+        });
+    }).catch(function(error) {
+        console.log("Transaction failed: ", error);
+    });
+}
+
 function sendMessage(subject, message) {
     var bID = document.cookie.substring(document.cookie.indexOf("=")+1);
     db.collection("businesses").doc(bID).get().then(function(doc) {
@@ -164,5 +211,145 @@ function sendMessage(subject, message) {
         }
     }).catch(function(error) {
         console.log("Error getting document:", error);
+    });
+}
+
+function addLocation(paremeters) {
+    var cID = document.cookie.substring(document.cookie.indexOf("=")+1);
+    var business = db.collection("businesses").doc(bID);
+    business.get().then(function(doc) {
+        if (doc.exists) {
+            var businessData = doc.data();
+            var lID = "l-" + generateID(10);
+            var newLocation = db.collection("locations").doc(lID);
+            console.log(lID + " reserved");
+            newLocation.get().then(function(doc) {
+                if (doc.exists) {
+                    console.log(lID + " exists");
+                    console.log("Duplicate ID. Trying again...");
+                    addLocation(paremeters);
+                } else {
+                    console.log(lID + " does not exist");
+                    newLocation.set({
+                        "BusinessName": businessData.BusinessName,
+                        "Logo": businessData.Logo,
+                        "Category": businessData.Category,
+                        "Address": {
+                            "Street": parameters.street,
+                            "City": parameters.city,
+                            "State": parameters.state,
+                            "Zip": parameters.zip,
+                            "String": parameters.street + ", " + parameters.city + ", " + parameters.state + " " + parameters.zip
+                        },
+                        "DateTime":{
+                            "StartString": parameters.startDate.toDateString(),
+                            "StopString": parameters.stopDate.toDateString(),
+                            "StartInt": parameters.startDate.getTime(),
+                            "StopInt": parameters.stopDate.getTime()
+                        },
+                        "Featured": parameters.photo,
+                        "Description": parameters.description,
+                        "lID": lID,
+                        "offers": parameters.offers,
+                        "bID": businessData.bID
+                    }).then(function() {
+                        db.runTransaction(function(transaction) {
+                            // This code may get re-run multiple times if there are conflicts.
+                            return transaction.get(business).then(function(businessDoc) {
+                                if (!businessDoc.exists) {
+                                    console.log("Document does not exist!");
+                                }
+                                var locations = businessDoc.data().Locations;
+                                locations.push(lID);
+                                transaction.update(business, {Locations: locations});
+                            });
+                        }).then(function() {
+                            console.log("Transaction successfully committed!");
+                            var interest = db.collection("interests").doc(businessData.Category);
+                            db.runTransaction(function(transaction) {
+                                // This code may get re-run multiple times if there are conflicts.
+                                return transaction.get(interest).then(function(interestDoc) {
+                                    if (!interestDoc.exists) {
+                                        console.log("Document does not exist!");
+                                    }
+                                    var locations = interestDoc.data().Locations;
+                                    locations.push(lID);
+                                    transaction.update(interest, {Locations: locations});
+                                });
+                            }).then(function() {
+                                console.log("Transaction successfully committed!");
+                                // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                            }).catch(function(error) {
+                                console.log("Transaction failed: ", error);
+                            });
+                        }).catch(function(error) {
+                            console.log("Transaction failed: ", error);
+                        });
+                    }).catch(function(error) {
+                        console.error("Error writing document: ", error);
+                    });
+                }
+            }).catch(function(error) {
+                console.log("Error getting document:", error);
+            });
+        } else {
+            console.log("ERROR! Docuemnt does not exist!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+}
+
+function displayBusinessData(bID) {
+    db.collection("businesses").doc(bID)
+    .onSnapshot(function(doc) {
+        if (!doc.exists) {
+            console.log("ERROR! Docuemnt does not exist!");
+        } else {
+            console.log("Current data: ", doc.data());
+        }
+    });
+}
+
+function displayClientData(cID) {
+    var oldMessageNumber;
+    db.collection("clients").doc(cID)
+    .onSnapshot(function(doc) {
+        if (!doc.exists) {
+            console.log("ERROR! Docuemnt does not exist!");
+        } else {
+            if (oldMessageNumber == undefined) {
+                oldMessageNumber = doc.data().Messages.length;
+            } else if (oldMessageNumber != doc.data().Messages.length) {
+                oldMessageNumber = doc.data().Messages.length;
+                alert("New Message!");
+            }
+            console.log("Current data: ", doc.data());
+        }
+    });
+}
+
+function login() {
+    var email = document.getElementById("userEmail").value();
+    var currentURL = location.href.substring(0,location.href.lastIndexOf("/"));
+    var found = false;
+    var userLoggingIn = db.collection("clients").where("Email","==",email)
+    userLoggingIn.get().then(function (querySnapshot) {
+    querySnapshot.forEach(function (doc) {
+        alert("Email found! In the future, you will receive this login link in an email. Currently, just go to " + currentURL + "?id=" + doc.data().cID + " to login as " + doc.data().Name + ".");
+        found = true;
+    }).then(function() {
+        userLoggingIn = db.collection("businesses").where("Email","==",email)
+        userLoggingIn.get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            alert("Email found! In the future, you will receive this login link in an email. Currently, just go to " + currentURL + "?id=" + doc.data().bID + " to login as " + doc.data().BusinessName + ".");
+            found = true;
+        }).then(function(){
+            if (!found) {
+                alert("No users with that email found!");
+            }
+        });
+        });
+    });
     });
 }
